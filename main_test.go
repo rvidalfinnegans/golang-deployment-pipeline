e2c322644b4e7f89f5523ab0a887a2166c5a9c57
rvidalfinnegans <43251471+rvidalfinnegans@users.noreply.github.com>
2018-09-18 18:07:17 -0300

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -29,7 +29,7 @@ func TestVersionHandler(t *testing.T) {
 	}
 
 	// Check the response body is what we expect.
-	expected := "que rompa"
+	expected := version
 	if rr.Body.String() != expected {
 		t.Errorf("handler returned unexpected body: got %v want %v",
 			rr.Body.String(), expected)
